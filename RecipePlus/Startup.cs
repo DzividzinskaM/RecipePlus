0b2ca981cc1261b29039b68ee7eb3ada2910f9a3
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-10-29 18:27:50 +0200

    change default action

--- RecipePlus/Startup.cs
+++ RecipePlus/Startup.cs
@@ -52,7 +52,7 @@ namespace RecipePlus
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Start}/{id?}");
             });
 
             /* app.UseDeveloperExceptionPage();
